62bc13d62b01b093b960ac802049c6de9fa713d6
Your Name <you@example.com>
2025-02-23 05:03:44 -0500

    add: fix config value

--- src/config-global.ts
+++ src/config-global.ts
@@ -1,15 +1,27 @@
 import packageJson from '../package.json';
 
-// ----------------------------------------------------------------------
-
 export type ConfigValue = {
   appName: string;
   appVersion: string;
+  workos: {
+    clientId: string;
+    apiHostname: string;
+  };
+  auth: {
+    storageTokenKeyName: string;
+  };
 };
-
-// ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
   appName: 'Minimal UI',
   appVersion: packageJson.version,
+  
+  workos: {
+    clientId: import.meta.env.VITE_WORKOS_CLIENT_ID || '',
+    apiHostname: import.meta.env.VITE_WORKOS_API_HOSTNAME || 'api.workos.com',
+  },
+
+  auth: {
+    storageTokenKeyName: 'accessToken',
+  },
 };
